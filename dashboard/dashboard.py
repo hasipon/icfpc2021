0759972193398acb3f8fc3bf053d8c3a09dcf752
Shingo INADA <mecha.is06@gmail.com>
2021-07-10 05:39:22 +0900

    update dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,8 +9,12 @@ from flask import Flask, request, render_template, abort
 
 static_path = pathlib.Path(__file__).resolve().parent / 'static'
 repo_path = pathlib.Path(__file__).resolve().parent.parent
+problems_path = repo_path / "problems"
 app = Flask(__name__, static_folder=str(static_path), static_url_path='')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# global cache
+problem_details = {}
 
 
 @app.after_request
@@ -21,44 +25,46 @@ def add_header(response):
     return response
 
 
+def load_problem_details(problem_files):
+    details = {}
+    for prob in problem_files:
+        with open(problems_path / prob) as fp:
+            details.update({prob: json.load(fp)})
+        details[prob]["base_score"] = 1000 * math.log2(
+            len(details[prob]["hole"]) *
+            len(details[prob]["figure"]["vertices"]) *
+            len(details[prob]["figure"]["edges"]) / 6.0)
+    return details
+
+
 @app.route('/')
 def index():
-    problems_path = repo_path / "problems"
     problems_json = json.loads((static_path / "problems.json").read_text(encoding='utf-8'))
-    dislikes = {
-        x[0]: (
-            int(x[1]) if x[1].isdigit() else '',
-            int(x[2]) if x[2].isdigit() else '',
-            (int(x[2]) + 1) / (int(x[1]) + 1) if x[1].isdigit() and x[2].isdigit() else 0,
-        ) for x in problems_json}
-
     problem_files = [os.path.relpath(x, problems_path) for x in glob.glob(str(problems_path / "*"))]
     problem_files.sort(key=lambda x: int(x))
 
-    # TODO cache
-    problem_detail = {}
-    for prob in problem_files:
-        with open(problems_path / prob) as fp:
-            problem_detail.update({prob: json.load(fp)})
-        problem_detail[prob]["base_score"] = 1000 * math.log2(
-            len(problem_detail[prob]["hole"]) *
-            len(problem_detail[prob]["figure"]["vertices"]) *
-            len(problem_detail[prob]["figure"]["edges"]) / 6.0)
+    global problem_details
+    if len(problem_details) != len(problem_files):
+        problem_details = load_problem_details(problem_files)
+
+    dislikes = {
+        x[0]: (
+            int(x[1]) if x[1].isdigit() else '',  # 自分のdislike
+            int(x[2]) if x[2].isdigit() else '',  # TOPのdislike
+            (int(x[2]) + 1) / (int(x[1]) + 1) if x[1].isdigit() and x[2].isdigit() else 0,
+        ) for x in problems_json}
 
     problems = [
         {
             "name": x,
-            "hole": len(problem_detail[x]["hole"]),
-            "eps": problem_detail[x]["epsilon"],
-            "edges": len(problem_detail[x]["figure"]["edges"]),
-            "vertices": len(problem_detail[x]["figure"]["vertices"]),
+            "hole": len(problem_details[x]["hole"]),
+            "eps": problem_details[x]["epsilon"],
+            "edges": len(problem_details[x]["figure"]["edges"]),
+            "vertices": len(problem_details[x]["figure"]["vertices"]),
             "dislike": dislikes[x][0],
-            "mindislike": dislikes[x][1],
             "dislike_ratio": dislikes[x][2],
-            "topscore": math.ceil(problem_detail[x]["base_score"]),
-            "score": math.ceil(problem_detail[x]["base_score"] * math.sqrt(
-                (dislikes[x][1] + 1) / (dislikes[x][0] + 1)
-            ) if dislikes[x][0] != '' else 0),
+            "topscore": math.ceil(problem_details[x]["base_score"]),
+            "score": math.ceil(problem_details[x]["base_score"] * math.sqrt(dislikes[x][2])),
         }
         for x in problem_files
     ]
