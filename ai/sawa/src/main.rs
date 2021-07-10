8f247f86917cd3106c553f45472aba89427e6c6b
shohei909 <909mmsphere@gmail.com>
2021-07-10 23:59:56 +0900

    improve auto

--- ai/sawa/src/main.rs
+++ ai/sawa/src/main.rs
@@ -1,6 +1,7 @@
 use std::env::args;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::Write;
 use serde_json::json;
 
 mod data;
@@ -11,21 +12,30 @@ use solve::solve;
 
 fn main()  -> std::io::Result<()>  {
     let arg:Vec<String> = args().collect();
-    let mut target = "8".to_owned();
-    if arg.len() > 1 {
-        target = arg[1].to_owned();
+    let cleared = vec![
+        4, 11, 12,13,15,16,17,18,20,21,22,23,24,25,26,34,35,38,39,41,43,
+        46,49,51,52,53,54,55,59,63,70,72,73,76
+    ];
+    for i in 1..89 {
+        if cleared.contains(&i) { continue; }
+        let target = format!("{}", i);
+        let file = File::open(format!("../../problems/{}", target))?;
+        let reader = BufReader::new(file);
+        let problem:Problem = serde_json::from_reader(reader).unwrap();
+        let result = solve(&problem);
+        
+
+        let meta = json!({ "valid": result.is_valid(), "score": result.get_score(), "dislike": result.dislike });
+        let answer = json!({ "vertices":result.answer.clone() });
+        println!("{}", target);
+        println!("{}", meta);
+        println!("{}", answer);
+        if result.is_valid() {
+            let mut file = File::create(format!("out/{}-sawa-auto4.json", target))?;
+            write!(file, "{}", answer);
+            let mut file = File::create(format!("out/{}-sawa-auto4.meta", target))?;
+            write!(file, "{}", meta);
+        }
     }
-    
-    let mut file = File::open(format!("../../problems/{}", target))?;
-    let reader = BufReader::new(file);
-    let problem:Problem = serde_json::from_reader(reader).unwrap();
-
-    let result = solve(&problem);
-    
-    println!("{}", json!(result.dislike));
-    println!("{}", json!(result.get_score()));
-    println!("{}", json!(result.is_valid()));
-    println!("{}", json!(Answer{ vertices:result.answer }));
-
     Ok(())
 }
--- ai/sawa/src/solve.rs
+++ ai/sawa/src/solve.rs
@@ -15,26 +15,30 @@ pub fn solve(problem:&Problem) -> State {
     let mut arr0 = Vec::new();
     let mut arr1 = Vec::new();
 
-    let size = 80;
+    let size = 700;
     for i in 0..size {
         let mut vertecies = current.answer.clone();
         if i > 3 { random(problem, &mut vertecies, 1, &mut rng); }
         arr0.push(State::new(problem, vertecies));
     }
 
-    for _ in 0..50 {
+    let mut prev_score = 1200003;
+    for _ in 0..80 {
         arr0.sort();
         arr0.split_off(size);
 
         for current in &arr0 {
             for i in 0..2 {
+                let score = current.get_score();
+
                 let mut vertecies = current.answer.clone();
-                if rng.gen_bool(0.1) { random(problem, &mut vertecies, 1, &mut rng); }
+                if rng.gen_bool(0.1) || score == prev_score && rng.gen_bool(0.9) { 
+                    random(problem, &mut vertecies, 1, &mut rng); 
+                }
                 if rng.gen_bool(0.6) { fit (problem, &mut vertecies, 1, &mut rng); }
                 pull(problem, &mut vertecies, 40, &mut rng);
 
                 let next = State::new(problem, vertecies);
-                println!("{} {} {}", current.is_valid(), current.dislike, json!(current.answer), );
                 if 
                     (!best.is_valid() && next.is_valid()) || 
                     (best.get_score() > next.get_score())
@@ -42,6 +46,7 @@ pub fn solve(problem:&Problem) -> State {
                     best = next.clone()
                 }
                 arr1.push(next);
+                prev_score = score;
             }
         }
         mem::swap(&mut arr0, &mut arr1);
