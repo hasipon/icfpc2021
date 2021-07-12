bcc6f7a2dd4b771b2e1f664965bea175ac9cc78d
Takuya Hashimoto <hasipon@users.noreply.github.com>
2021-07-12 12:18:37 +0000

    cp

---
+++ submit/eval/core.go
@@ -0,0 +1,389 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+type Int = big.Int
+
+type Figure struct {
+	Edges    [][]int  `json:"edges"`
+	Vertices [][]*Int `json:"vertices"`
+}
+
+type ProblemBonus struct {
+	Bonus    string `json:"bonus"`
+	Problem  int    `json:"problem"`
+	Position []*Int `json:"position"`
+}
+
+type Problem struct {
+	Hole            [][]*Int       `json:"hole"`
+	Epsilon         *Int           `json:"epsilon"`
+	Figure          Figure         `json:"figure"`
+	Bonuses         []ProblemBonus `json:"bonuses"`
+	WallHacked      bool
+	Globalist       bool
+	SuperFlex bool
+	OriginalEdgeNum int
+}
+
+type Point = []*Int
+
+func dot(a, b Point) *Int {
+	x := new(Int).Mul(a[0], b[0])
+	y := new(Int).Mul(a[1], b[1])
+	return new(Int).Add(x, y)
+}
+
+func det(a, b Point) *Int {
+	x := new(Int).Mul(a[0], b[1])
+	y := new(Int).Mul(a[1], b[0])
+	return new(Int).Sub(x, y)
+}
+
+var zero = new(Int).SetInt64(0)
+
+const (
+	FRONT = 1
+	RIGHT = 2
+	BACK  = 4
+	LEFT  = 8
+	ON    = 16
+)
+
+func ccw(a, b, c Point) int {
+	b_a := Point{new(Int).Sub(b[0], a[0]), new(Int).Sub(b[1], a[1])}
+	c_a := Point{new(Int).Sub(c[0], a[0]), new(Int).Sub(c[1], a[1])}
+	s := det(b_a, c_a)
+	if s == zero {
+		return ON
+	}
+	if s.Cmp(zero) < 0 {
+		return RIGHT
+	}
+	return LEFT
+}
+
+func intersect(p []Point) bool {
+	sub := func(i, j, k, l int) *Int {
+		i--
+		k--
+		return new(Int).Sub(p[i][j], p[k][l])
+	}
+	tc1_A := new(Int).Mul(sub(1, 0, 2, 0), sub(3, 1, 1, 1))
+	tc1_B := new(Int).Mul(sub(1, 1, 2, 1), sub(1, 0, 3, 0))
+	tc1 := new(Int).Add(tc1_A, tc1_B)
+
+	tc2_A := new(Int).Mul(sub(1, 0, 2, 0), sub(4, 1, 1, 1))
+	tc2_B := new(Int).Mul(sub(1, 1, 2, 1), sub(1, 0, 4, 0))
+	tc2 := new(Int).Add(tc2_A, tc2_B)
+
+	td1_A := new(Int).Mul(sub(3, 0, 4, 0), sub(1, 1, 3, 1))
+	td1_B := new(Int).Mul(sub(3, 1, 4, 1), sub(3, 0, 1, 0))
+	td1 := new(Int).Add(td1_A, td1_B)
+
+	td2_A := new(Int).Mul(sub(3, 0, 4, 0), sub(2, 1, 3, 1))
+	td2_B := new(Int).Mul(sub(3, 1, 4, 1), sub(3, 0, 2, 0))
+	td2 := new(Int).Add(td2_A, td2_B)
+
+	tc := new(Int).Mul(tc1, tc2)
+	td := new(Int).Mul(td1, td2)
+	if tc.Cmp(new(Int)) < 0 && td.Cmp(new(Int)) < 0 {
+		return true
+	}
+	return false
+}
+
+func distance(a []*Int, b []*Int) *Int {
+	var diffX, diffY, XX, YY Int
+	diffX.Sub(a[0], b[0])
+	diffY.Sub(a[1], b[1])
+	XX.Mul(&diffX, &diffX)
+	YY.Mul(&diffY, &diffY)
+	var sum Int
+	sum.Add(&XX, &YY)
+	return &sum
+}
+
+type Bonus struct {
+	Bonus   string `json:"bonus,omitempty"`
+	Problem int    `json:"problem,omitempty"`
+	Edge    []int  `json:"edge,omitempty"`
+}
+
+type Pose struct {
+	Vertices [][]*Int `json:"vertices,omitempty"`
+	Bonuses  []*Bonus `json:"bonuses,omitempty"`
+}
+
+func dislike(problem *Problem, pose *Pose) *Int {
+	sum := new(Int)
+	for _, h := range problem.Hole {
+		first := true
+		min := new(Int)
+		for _, v := range pose.Vertices {
+			tmp := distance(h, v)
+			if first {
+				min = tmp
+			} else {
+				if tmp.Cmp(min) < 0 {
+					min = tmp
+				}
+			}
+			first = false
+		}
+		sum.Add(sum, min)
+	}
+	return sum
+}
+
+func include(problem *Problem, p Point) bool {
+	x := p[0]
+	y := p[1]
+	cnt := 0
+	for i, _ := range problem.Hole {
+		j := (i + 1) % len(problem.Hole)
+		x0 := new(Int).Set(problem.Hole[i][0])
+		y0 := new(Int).Set(problem.Hole[i][1])
+		x1 := new(Int).Set(problem.Hole[j][0])
+		y1 := new(Int).Set(problem.Hole[j][1])
+
+		x0 = x0.Sub(x0, x)
+		y0 = y0.Sub(y0, y)
+		x1 = x1.Sub(x1, x)
+		y1 = y1.Sub(y1, y)
+
+		cv := new(Int).Add(new(Int).Mul(x0, x1), new(Int).Mul(y0, y1))
+		sv := new(Int).Sub(new(Int).Mul(x0, y1), new(Int).Mul(x1, y0))
+		if sv.Cmp(zero) == 0 && cv.Cmp(zero) <= 0 {
+			return true
+		}
+
+		if y0.Cmp(y1) < 0 {
+		} else {
+			tmp := x0
+			x0 = x1
+			x1 = tmp
+			tmp = y0
+			y0 = y1
+			y1 = tmp
+		}
+
+		if y0.Cmp(zero) <= 0 && zero.Cmp(y1) < 0 {
+			a := new(Int).Mul(x0, new(Int).Sub(y1, y0))
+			b := new(Int).Mul(y0, new(Int).Sub(x1, x0))
+			if b.Cmp(a) < 0 {
+				cnt++
+			}
+		}
+	}
+	return cnt%2 == 1
+}
+
+func applyBonus(problem *Problem, pose *Pose) *Problem {
+	problem.OriginalEdgeNum = len(problem.Figure.Edges)
+	for _, b := range pose.Bonuses {
+		if b.Bonus == "BREAK_A_LEG" {
+			target := make(map[int]bool)
+			v1 := problem.Figure.Vertices[b.Edge[0]]
+			v2 := problem.Figure.Vertices[b.Edge[1]]
+			target[b.Edge[0]] = true
+			target[b.Edge[1]] = true
+			mid := Point{
+				new(Int).Div(new(Int).Add(v1[0], v2[0]), new(Int).SetInt64(2)),
+				new(Int).Div(new(Int).Add(v1[1], v2[1]), new(Int).SetInt64(2)),
+			}
+			newVertexId := len(problem.Figure.Vertices)
+			problem.Figure.Vertices = append(problem.Figure.Vertices, mid)
+			newEdges := [][]int{
+				[]int{b.Edge[0], newVertexId},
+				[]int{newVertexId, b.Edge[1]},
+			}
+			for _, e := range problem.Figure.Edges {
+				if target[e[0]] && target[e[1]] {
+					continue
+				}
+				newEdges = append(newEdges, e)
+			}
+			problem.Figure.Edges = newEdges
+		} else if b.Bonus == "GLOBALIST" {
+			problem.Globalist = true
+		} else if b.Bonus == "WALLHACK" {
+			problem.WallHacked = true
+		} else if b.Bonus == "SUPERFLEX"{
+			problem.SuperFlex = true
+		} else {
+			log.Printf("Unknown bounus: %s", b.Bonus)
+		}
+	}
+	return problem
+}
+
+func validate(problem *Problem, pose *Pose) (bool, string) {
+	if len(pose.Bonuses) > 1 {
+		return false, "too many bonuses"
+	}
+
+	origV := problem.Figure.Vertices
+	nowV := pose.Vertices
+	if len(origV) != len(nowV) {
+		return false, "mismatch length"
+	}
+
+	outsideVerticesCnt := make(map[int]bool)
+
+	for i, p := range pose.Vertices {
+		if !include(problem, p) {
+			outsideVerticesCnt[i] = true
+		}
+	}
+
+	numOfAllowOutVertex := 0
+	if problem.WallHacked {
+		numOfAllowOutVertex++
+	}
+
+	if len(outsideVerticesCnt) > numOfAllowOutVertex {
+		ids := ""
+		cnt := 0
+		for k, _ := range outsideVerticesCnt {
+			ids += fmt.Sprintf("%d,", k)
+			cnt++
+			if cnt > numOfAllowOutVertex {
+				break
+			}
+		}
+		return false, fmt.Sprintf("vertex(%s)(0-based) is out of the hole", ids)
+	}
+
+	holePointsInd := make(map[string]int)
+	for i, h := range problem.Hole {
+		holePointsInd[h[0].String()+","+h[1].String()] = i
+	}
+
+	outEdges := [][]int{}
+
+	for _, e := range problem.Figure.Edges {
+		v1 := pose.Vertices[e[0]]
+		v2 := pose.Vertices[e[1]]
+		two := new(Int).SetInt64(2)
+		sumX := new(Int).Mul(two, new(Int).Add(v1[0], v2[0]))
+		sumY := new(Int).Mul(two, new(Int).Add(v1[1], v2[1]))
+		mid := Point{new(Int).Div(sumX, two), new(Int).Div(sumY, two)}
+		for _, v := range problem.Hole {
+			v[0] = v[0].Mul(v[0], two)
+			v[1] = v[1].Mul(v[1], two)
+		}
+		midInc := include(problem, mid)
+		for _, v := range problem.Hole {
+			v[0] = v[0].Div(v[0], two)
+			v[1] = v[1].Div(v[1], two)
+		}
+		if !midInc {
+			outEdges = append(outEdges, e)
+		}
+	}
+
+	var globalEpsSum float64
+	cntFlex := 0
+	for _, e := range problem.Figure.Edges {
+		i := e[0]
+		j := e[1]
+		origD := distance(origV[i], origV[j])
+		nowD := distance(nowV[i], nowV[j])
+		if problem.Globalist {
+			nowDf, _ := new(big.Float).SetInt(nowD).Float64()
+			origDf, _ := new(big.Float).SetInt(origD).Float64()
+			globalEpsSum += math.Abs(nowDf/origDf-1) * 1000000
+		} else {
+			var diff *Int
+			if nowD.Cmp(origD) >= 0 {
+				diff = new(Int).Sub(nowD, origD)
+			} else {
+				diff = new(Int).Sub(origD, nowD)
+			}
+			diff = diff.Mul(diff, new(Int).SetInt64(1000000))
+			eps := new(Int).Mul(problem.Epsilon, origD)
+			result := diff.Cmp(eps)
+			if result > 0 {
+				if problem.SuperFlex {
+					cntFlex++
+					if cntFlex >1 {
+						return false, fmt.Sprintf("Edge between (%d, "+
+							"%d) has an invalid length: original: %d pose: %d", i, j, origD, nowD)
+					}
+				}else {
+					return false, fmt.Sprintf("Edge between (%d, "+
+						"%d) has an invalid length: original: %d pose: %d", i, j, origD, nowD)
+				}
+			}
+		}
+		for ii, _ := range problem.Hole {
+			h := problem.Hole
+			jj := (ii + 1) % len(h)
+			if intersect([]Point{h[ii], h[jj], nowV[i], nowV[j]}) {
+				outEdges = append(outEdges, e)
+			}
+		}
+	}
+	if problem.Globalist {
+		epsLimit, _ := new(big.Float).SetInt(problem.Epsilon).Float64()
+		epsLimit = epsLimit * float64(problem.OriginalEdgeNum)
+		if globalEpsSum > epsLimit {
+			return false, fmt.Sprintf("global epsilon budget exceeded. limit %f, now %f",
+				epsLimit, globalEpsSum)
+		}
+	}
+	if problem.WallHacked {
+		if len(outsideVerticesCnt) == 1 {
+			hackedVertex := -1
+			for k, _ := range outsideVerticesCnt {
+				hackedVertex = k
+				for _, e := range outEdges {
+					if e[0] != k && e[1] != k {
+						return false, fmt.Sprintf("Invalid edge between (%d, "+
+							"%d)", e[0], e[1])
+					}
+				}
+			}
+			return true, fmt.Sprintf("OK: hacked vertex is %d(0-based)", hackedVertex)
+		} else {
+			cnt := make(map[int]int)
+			for _, e := range outEdges {
+				cnt[e[0]]++
+				cnt[e[1]]++
+			}
+			found := false
+			hackedVertex := -1
+			for k, v := range cnt {
+				if v == len(outEdges) {
+					found = true
+					hackedVertex = k
+				}
+				if problem.SuperFlex && v == len(outEdges) -1{
+					found = true
+					hackedVertex = k
+				}
+			}
+			if !found {
+				return false, fmt.Sprintf("Too many Invalid edges")
+			} else {
+				return true, fmt.Sprintf("OK: hacked vertex is %d(0-based)", hackedVertex)
+			}
+		}
+	} else {
+		if len(outEdges) == 0 {
+			return true, "OK"
+		} else {
+			return false, fmt.Sprintf("invalid edge(%d, %d)(0-based)", outEdges[0][0],
+				outEdges[0][1])
+		}
+		// else if len(outEdges) == 1 && problem.SuperFlex {
+		//	return true, fmt.Sprintf("OK, flex edge(%d, %d)(0-based)", outEdges[0][0],
+		//		outEdges[0][1])
+	}
+}
---
+++ submit/eval/main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+)
+
+//go:embed problems/*
+var problems embed.FS
+
+func eval(problemBytes, poseBytes []byte) (string, bool, string) {
+	var problem *Problem
+	if err := json.Unmarshal(problemBytes, &problem); err != nil {
+		log.Fatal("problem:", err)
+	}
+
+	var pose *Pose
+	if err := json.Unmarshal(poseBytes, &pose); err != nil {
+		log.Fatal("pose:", err)
+	}
+	problem = applyBonus(problem, pose)
+	result := dislike(problem, pose)
+	valid, msg := validate(problem, pose)
+	return result.String(), valid, msg
+
+}
+func getProblem(id string) ([]byte, error) {
+	p, err := problems.ReadFile("problems/" + id)
+	return p, err
+}
+
+func convertAll() {
+	var err error
+	for i := 1; ; i++ {
+		id := fmt.Sprintf("%d", i)
+		_, err = getProblem(id)
+		if err != nil {
+			break
+		}
+		result := convert("", id)
+		err = ioutil.WriteFile("./converted_problems/"+id, result, 0700)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func convert(problemFile, problemId string) []byte {
+	var problemBytes []byte
+	var err error
+	if problemFile != "" {
+		problemBytes, err = ioutil.ReadFile(problemFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if problemId != "" {
+		problemBytes, err = getProblem(problemId)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal("./eval --convert --problem-file <filename>\n" +
+			"./eval --convert --problem-id <id>\n")
+	}
+
+	var problem Problem
+	if err := json.Unmarshal(problemBytes, &problem); err != nil {
+		log.Fatal(err)
+	}
+
+	n := len(problem.Hole)
+	en := len(problem.Figure.Edges)
+	vn := len(problem.Figure.Vertices)
+
+	var ret bytes.Buffer
+	ret.WriteString(fmt.Sprintf("%d %d %d %s\n", n, en, vn, problem.Epsilon.String()))
+
+	for _, h := range problem.Hole {
+		ret.WriteString(fmt.Sprintf("%s %s\n", h[0].String(), h[1].String()))
+	}
+	for _, e := range problem.Figure.Edges {
+		ret.WriteString(fmt.Sprintf("%d %d\n", e[0], e[1]))
+	}
+	for _, v := range problem.Figure.Vertices {
+		ret.WriteString(fmt.Sprintf("%s %s\n", v[0].String(), v[1].String()))
+	}
+	return ret.Bytes()
+}
+
+func cli(problemFile, problemId, poseFile string) {
+	var problemBytes []byte
+	var err error
+	if problemFile != "" {
+		problemBytes, err = ioutil.ReadFile(problemFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if problemId != "" {
+		problemBytes, err = getProblem(problemId)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal("./eval --problem-file <filename> --pose-file <poseFile>\n" +
+			"./eval --problem-id <id> --pose-file <poseFile>\n")
+	}
+	poseBytes, err := ioutil.ReadFile(poseFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dislike, valid, msg := eval(problemBytes, poseBytes)
+	if valid {
+		fmt.Println(dislike)
+		fmt.Println("valid")
+	} else {
+		fmt.Println(-1)
+		fmt.Println(msg)
+	}
+}
+
+func main() {
+	/*
+		inputProblem := "{\"hole\":[[45,80],[35,95],[5,95],[35,50],[5,5],[35,5],[95,95],[65,95],[55," +
+			"80]],\"epsilon\":150000,\"figure\":{\"edges\":[[2,5],[5,4],[4,1],[1,0],[0,8],[8,3],[3,7],[7,11],[11,13],[13,12],[12,18],[18,19],[19,14],[14,15],[15,17],[17,16],[16,10],[10,6],[6,2],[8,12],[7,9],[9,3],[8,9],[9,12],[13,9],[9,11],[4,8],[12,14],[5,10],[10,15]],\"vertices\":[[20,30],[20,40],[30,95],[40,15],[40,35],[40,65],[40,95],[45,5],[45,25],[50,15],[50,70],[55,5],[55,25],[60,15],[60,35],[60,65],[60,95],[70,95],[80,30],[80,40]]}}"
+		poseText := "{\n\"vertices\": [\n[21, 28], [31, 28], [31, 87], [29, 41], [44, 43], [58, 70],\n[38, 79], [32, 31], [36, 50], [39, 40], [66, 77], [42, 29],\n[46, 49], [49, 38], [39, 57], [69, 66], [41, 70], [39, 60],\n[42, 25], [40, 35]\n]\n}"
+	*/
+	var server = flag.String("server", "", "http server mode(specify listen addr)")
+	var problemId = flag.String("problem-id", "", "problem id")
+	var problemFile = flag.String("problem-file", "", "problem file")
+	var poseFile = flag.String("pose-file", "", "pose file")
+	var batch = flag.Bool("batch", false, "batch mode")
+	var solutions = flag.String("solutions", "solutions", "solutions directory")
+	var submit = flag.Bool("submit", false, "auto submit (batch mode)")
+	var convertFlag = flag.Bool("convert", false, "input convert")
+	var allFlag = flag.Bool("all", false, "convert all")
+	var dbpath = flag.String("db", "", "sqlitedb path")
+	var initdb = flag.Bool("initdb", false, "init db")
+	var solutionsToDB = flag.Bool("solutions-to-db", false, "insert solution from solution folder")
+	var migratedb = flag.Bool("migratedb", false, "migrate db")
+	flag.Parse()
+	if *dbpath != "" {
+		conn, err := sqlx.Open("sqlite3", *dbpath)
+		if err != nil {
+			log.Fatal("failed to open database", err)
+		}
+		defaultDB = SQLiteDB{DB: conn}
+
+		if *initdb {
+			defaultDB.Init()
+		}
+
+		if *migratedb {
+			log.Println("migrate:", defaultDB.Migrate())
+		}
+
+		if *solutionsToDB {
+			registerSolutionInDirectory(*solutions)
+		}
+	}
+
+	if *server != "" {
+		fmt.Println("server mode")
+		if *batch {
+			fmt.Println("also batch mode")
+			go batchMode(*solutions, *submit)
+		}
+
+		http.HandleFunc("/health", func(w http.ResponseWriter, request *http.Request) {
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte("ok"))
+		})
+		http.HandleFunc("/eval/", func(w http.ResponseWriter, request *http.Request) {
+			id := strings.TrimPrefix(request.URL.Path, "/eval/")
+			log.Printf("id = %s\n", id)
+			body, err := ioutil.ReadAll(request.Body)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(500)
+				_, _ = w.Write([]byte(err.Error()))
+				return
+			}
+			problem, err := getProblem(id)
+			if err != nil {
+				log.Print(err)
+				w.WriteHeader(500)
+				_, _ = w.Write([]byte(err.Error()))
+				return
+			}
+			result, valid, msg := eval(problem, body)
+			if !valid {
+				result = "-1"
+			}
+
+			if *batch && valid {
+				fileName := fmt.Sprintf("%v-eval-dislike%v", id, result)
+				filePath := path.Join(*solutions, fileName)
+				_, err = os.Stat(filePath)
+				if os.IsNotExist(err) {
+					os.WriteFile(filePath, body, 0644)
+				}
+				if defaultDB.Ok() {
+					idInt, _ := strconv.Atoi(id)
+					_, err = defaultDB.RegisterSolution(fmt.Sprintf("eval-dislike%v", result), idInt, body)
+					if err != nil {
+						log.Println("RegisterSolution err", err)
+					}
+				}
+			}
+
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"dislike\": %s, \"valid\": %t, \"msg\": \"%s\"}",
+				result, valid, msg)))
+		})
+		for {
+			err := http.ListenAndServe(*server, nil)
+			log.Print(err)
+		}
+	} else if *batch {
+		batchMode(*solutions, *submit)
+	} else if *allFlag {
+		convertAll()
+	} else if *convertFlag {
+		fmt.Print(string(convert(*problemFile, *problemId)))
+	} else {
+		cli(*problemFile, *problemId, *poseFile)
+	}
+
+}
